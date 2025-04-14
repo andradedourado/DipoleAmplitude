1bbc79878411b71a1bb52c21241804ef0e6b2651
Luciana Andrade Dourado <luciana.dourado@gssi.it>
2025-04-14 21:33:46 +0200

    Minor changes

--- local-sources/scripts/compute_spectrum.py
+++ local-sources/scripts/compute_spectrum.py
@@ -5,8 +5,8 @@ with open("simulations_dir.txt", "r") as f:
 
 RESULTS_DIR = "../results"
 
-ESS = np.delete(np.logspace(0, 4, num = 81), 0)
-ES = np.logspace(4./(2.*(len(ESS) - 1)), 4. - 4./(2.*(len(ESS) - 1)), num = len(ESS) - 1)
+ENERGY_EDGES = np.logspace(0, 4, num = 80)
+ENERGY_BIN_CENETRS = np.logspace(4./(2.*(len(ENERGY_EDGES) - 1)), 4. - 4./(2.*(len(ENERGY_EDGES) - 1)), num = len(ENERGY_EDGES) - 1)
 GALAXIES = ['CenA', 'ForA', 'VirA', \
             'NGC253', 'M82', 'NGC4945', 'M83', 'IC342', 'NGC6946', 'NGC2903', 'NGC5055', 'NGC3628', 'NGC3627', \
             'NGC4631', 'NGC891', 'NGC3556', 'NGC660', 'NGC2146', 'NGC3079', 'NGC1068', 'NGC1365']
@@ -38,18 +38,17 @@ def w_spec(Es, Zs):
 def compute_spectrum(EGMF, galaxy, Zs):
 
     data = np.loadtxt(f"{SIMULATIONS_DIR}/{PARTICLES[iZs(Zs)]}/events_{galaxy}_EGMF{EGMF:02d}.txt")
-    hist, bin_edges = np.histogram(data[:,2], bins = np.logspace(0, 4, num = 80), weights = w_sim(data[:,10]) * w_spec(data[:,10], Zs))
-    return hist
+    return np.histogram(data[:,2], bins = ENERGY_EDGES, weights = w_sim(data[:,10]) * w_spec(data[:,10], Zs))[0]
 
 # ----------------------------------------------------------------------------------------------------
 def write_spectrum(galaxy, Zs):
 
-    spec = np.zeros_like(ES)
+    spec = np.zeros_like(ENERGY_BIN_CENETRS)
 
     for EGMF in range(20):
         spec += compute_spectrum(EGMF, galaxy, Zs)  
 
-    np.savetxt(f"{RESULTS_DIR}/spec_{galaxy}_{PARTICLES[iZs(Zs)]}.dat", np.column_stack((ES * 1e18, spec / (ES * 1e18))), fmt = "%.15e")
+    np.savetxt(f"{RESULTS_DIR}/spec_{galaxy}_{PARTICLES[iZs(Zs)]}.dat", np.column_stack((ENERGY_BIN_CENETRS * 1e18, spec / (ENERGY_BIN_CENETRS * 1e18))), fmt = "%.15e")
   
 # ----------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
