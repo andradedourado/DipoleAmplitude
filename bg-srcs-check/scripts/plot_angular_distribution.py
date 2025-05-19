25bb330f15fcde453eb90dbfa025e18466ef8f6d
Luciana Andrade Dourado <luciana.dourado@gssi.it>
2025-05-19 16:53:24 +0200

    Compute dN/dcosθ for several values of scattering length over source distance

--- bg-srcs-check/scripts/plot_angular_distribution.py
+++ bg-srcs-check/scripts/plot_angular_distribution.py
@@ -15,21 +15,21 @@ REFERENCES_DIR = "../references"
 RESULTS_DIR = "../results"
 
 # ----------------------------------------------------------------------------------------------------
-def plot_angular_distribution():
+def plot_angular_distribution(): # Figure 2
 
     for i, lbd_scatt_over_rs in enumerate([0.01, 0.03, 0.1, 0.3, 1, 3]):
 
         lbd_scatt_over_rs_str = str(lbd_scatt_over_rs).rstrip('0').rstrip('.').replace('.', '_')
 
-        data_LAD = np.loadtxt(f"{RESULTS_DIR}/angular_distr_{lbd_scatt_over_rs_str}.dat")
+        data_LAD = np.loadtxt(f"{RESULTS_DIR}/angular_distr_{lbd_scatt_over_rs_str}_27Mpc.dat")
         data_Lang2021 = np.loadtxt(f"{REFERENCES_DIR}/Lang2021_dN_dcostheta_{lbd_scatt_over_rs_str}.dat")
 
-        plt.plot(data_LAD[:,0], data_LAD[:,1], c = cm.OrRd(np.linspace(0, 1, 20)[15 - 2 * i]), label = lbd_scatt_over_rs)
-        plt.plot(data_Lang2021[:,0], data_Lang2021[:,1], c = cm.OrRd(np.linspace(0, 1, 20)[15 - 2 * i]), ls = '--')
+        plt.plot(data_LAD[:,0], 2 * data_LAD[:,1], c = cm.OrRd(np.linspace(0, 1, 20)[15 - 2 * i]), label = lbd_scatt_over_rs)
+        # plt.plot(data_Lang2021[:,0], data_Lang2021[:,1], c = cm.OrRd(np.linspace(0, 1, 20)[15 - 2 * i]), ls = '--')
 
-    LAD = lines.Line2D([], [], c = 'k', ls = '-', label = 'LAD')
-    Lang2021 = lines.Line2D([], [], c = 'k', ls = '--', label = 'Lang+21')
-    plt.gca().add_artist(plt.legend(title = 'Results', handles = [LAD, Lang2021], frameon = True, loc = 'upper left', bbox_to_anchor = (0.425, 1.)))
+    # LAD = lines.Line2D([], [], c = 'k', ls = '-', label = 'LAD')
+    # Lang2021 = lines.Line2D([], [], c = 'k', ls = '--', label = 'Lang+21')
+    # plt.gca().add_artist(plt.legend(title = 'Results', handles = [LAD, Lang2021], frameon = True, loc = 'upper left', bbox_to_anchor = (0.425, 1.)))
 
     plt.xlabel(r'$\cos{\theta}$')
     plt.ylabel(r'$dN/d\cos{\theta}$')
