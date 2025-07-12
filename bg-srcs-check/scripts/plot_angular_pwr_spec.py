bb79f90a2afd59ca22830e11b74cbc37adb8137c
Luciana Andrade Dourado <luciana.dourado@gssi.it>
2025-07-12 12:48:56 +0200

    Add plot of angular power spectrum as a function of scattering length to source distance ratio

--- bg-srcs-check/scripts/plot_angular_pwr_spec.py
+++ bg-srcs-check/scripts/plot_angular_pwr_spec.py
@@ -18,17 +18,21 @@ RESULTS_DIR = "../results"
 def plot_angular_power_spectrum():
 
     for l in range(5):
-        data = np.loadtxt(f"{REFERENCES_DIR}/Lang2021_Cl_lbd_scatt_over_rs_{l}.dat")
-        plt.plot(data[:,0], data[:,1], c = cm.Greens(np.linspace(0, 1, 20)[19-3*l]), label = r'$l = {}$'.format(l))
+        LAD_data = np.loadtxt(f"{RESULTS_DIR}/angular_pwr_spec_27Mpc_{l:02d}.dat")
+        Lang2021_data = np.loadtxt(f"{REFERENCES_DIR}/Lang2021_Cl_lbd_scatt_over_rs_{l:02d}.dat")
+        plt.plot(LAD_data[:,0], np.sqrt(LAD_data[:,1]), c = cm.Greens(np.linspace(0, 1, 20)[19-3*l]), label = r'$l = {}$'.format(l))
+        plt.plot(Lang2021_data[:,0], Lang2021_data[:,1], c = cm.Greens(np.linspace(0, 1, 20)[19-3*l]), ls = '--', label = '_nolegend_')
 
-    data = np.loadtxt(f"{REFERENCES_DIR}/Lang2021_Cl_lbd_scatt_over_rs_f_non-dip.dat")
-    plt.plot(data[:,0], data[:,1], c = 'k', ls = '--', label = r'$f_{\rm non-dip}$')
+
+    # Lang2021_data = np.loadtxt(f"{REFERENCES_DIR}/Lang2021_Cl_lbd_scatt_over_rs_f_non-dip.dat")
+    # plt.plot(Lang2021_data[:,0], Lang2021_data[:,1], c = 'k', ls = '--', label = r'$f_{\rm non-dip}$')
 
     at = AnchoredText(r'$r_s = 27 \: \rm Mpc$', loc = 'lower right', frameon = False, prop = {'fontsize': 'large'})
     plt.gca().add_artist(at)
 
     plt.xscale('log')
     plt.yscale('log')
+    plt.ylim(9e-4, 2e0)
     plt.xlabel(r'$\lambda_{\rm scatt}/r_s$')
     plt.ylabel(r'$\sqrt{C_l}$')
     plt.legend()
