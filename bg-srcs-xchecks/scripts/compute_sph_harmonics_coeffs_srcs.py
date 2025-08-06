2ba19990cf2dff49ca462484bebe81bbf794b2cf
Luciana Andrade Dourado <luciana.dourado@gssi.it>
2025-08-06 15:34:34 +0200

    Prevent NaN and Inf values in Equation (15) computation

--- bg-srcs-xchecks/scripts/compute_sph_harmonics_coeffs_srcs.py
+++ bg-srcs-xchecks/scripts/compute_sph_harmonics_coeffs_srcs.py
@@ -47,7 +47,8 @@ def compute_sph_harmonics_coeffs(l, iE, rs, Z):
         data = np.loadtxt(f"{SIMULATIONS_DIR}/{PARTICLES[iZ(Z)]}/{int(rs)}Mpc/angular_distr_{iE:02d}.dat")
 
     N = (simps(data[:,1], data[:,0]) + d2N_dAdt_bal(ES[iE], rs, Z)) / 2
-    x, integrand = data[:,0], legendre(l)(data[:,0]) * data[:,1]
+    x, integrand = data[:,0], legendre(l)(data[:,0]) * data[:,1]        
+
     return (2*l + 1) / 2 / N * (simps(integrand, x) + d2N_dAdt_bal(ES[iE], rs, Z) * legendre(l)(1))
 
 # ----------------------------------------------------------------------------------------------------
@@ -59,11 +60,17 @@ def Phi_l_tot(l, Dmin, Z):
     Phi_l_tot = np.zeros_like(ES)
 
     for irs, rs in enumerate(np.arange(Dmin, 10**3.5, Dmin)):
+        
         if irs + 1 >= spec.shape[1]:
             break
+        
         Phi_l = np.zeros_like(ES)
+        
         for iE in range(len(ES)):
-            Phi_l[iE] = compute_sph_harmonics_coeffs(l, iE, rs, Z)
+            Phi_l_temp = compute_sph_harmonics_coeffs(l, iE, rs, Z)
+            if np.isfinite(Phi_l_temp):
+                Phi_l[iE] = Phi_l_temp
+
         Phi_l_tot += (Phi_l * spec[:,irs+1] / (irs + 1))**2
 
     return spec[:,0], np.sqrt(Phi_l_tot)
