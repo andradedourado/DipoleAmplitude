93f462c03bacf9b54e4ebb3f0b63c42f2f3f06df
Luciana Andrade Dourado <luciana.dourado@gssi.it>
2025-08-13 22:19:40 +0200

    Calculate monopole term for different magnetic fields

--- bg-srcs-xchecks/scripts/compute_sph_harmonics_coeffs_srcs.py
+++ bg-srcs-xchecks/scripts/compute_sph_harmonics_coeffs_srcs.py
@@ -11,7 +11,6 @@ ES = ES[MASK]
 PARTICLES = ['1H', '4He', '14N', '28Si', '56Fe']
 ZS = [1, 2, 7, 14, 26]
 
-B = 1 # nG 
 lbd_coh = 1 # Mpc
 
 # ----------------------------------------------------------------------------------------------------
@@ -23,7 +22,7 @@ def iZ(Z):
         raise ValueError(f"Z ({Z}) not found in ZS.")
     
 # ----------------------------------------------------------------------------------------------------
-def scattering_length(E, Z):
+def scattering_length(B, E, Z):
 
     RL = 1.081 / Z * E / B # Mpc; E in EeV and B in nG 
 
@@ -34,27 +33,27 @@ def scattering_length(E, Z):
         return (RL/lbd_coh)**2 * lbd_coh # Mpc  
 
 # ----------------------------------------------------------------------------------------------------
-def d2N_dAdt_bal(E, rs, Z): # Equation (5)
+def d2N_dAdt_bal(B, E, rs, Z): # Equation (5)
 
-    return np.exp(- rs / scattering_length(E, Z)) / (4 * np.pi * rs**2)
+    return np.exp(- rs / scattering_length(B, E, Z)) / (4 * np.pi * rs**2)
 
 # ----------------------------------------------------------------------------------------------------
-def compute_sph_harmonics_coeffs(l, iE, rs, Z):
+def compute_sph_harmonics_coeffs(l, B, iE, rs, Z):
 
     if Z == 7:
-        data = np.loadtxt(f"{SIMULATIONS_DIR}/{PARTICLES[iZ(Z)]}/1nG/{int(rs)}Mpc/angular_distr_{iE:02d}.dat")
+        data = np.loadtxt(f"{SIMULATIONS_DIR}/{PARTICLES[iZ(Z)]}/{format_filename_value(B)}nG/{int(rs)}Mpc/angular_distr_{iE:02d}.dat")
     else:
         data = np.loadtxt(f"{SIMULATIONS_DIR}/{PARTICLES[iZ(Z)]}/{int(rs)}Mpc/angular_distr_{iE:02d}.dat")
 
-    N = (simps(data[:,1], data[:,0]) + d2N_dAdt_bal(ES[iE], rs, Z)) / 2
+    N = (simps(data[:,1], data[:,0]) + d2N_dAdt_bal(B, ES[iE], rs, Z)) / 2
     x, integrand = data[:,0], legendre(l)(data[:,0]) * data[:,1]        
 
-    return (2*l + 1) / 2 / N * (simps(integrand, x) + d2N_dAdt_bal(ES[iE], rs, Z) * legendre(l)(1))
+    return (2*l + 1) / 2 / N * (simps(integrand, x) + d2N_dAdt_bal(B, ES[iE], rs, Z) * legendre(l)(1))
 
 # ----------------------------------------------------------------------------------------------------
-def Phi_l_tot(l, Dmin, Z):
+def Phi_l_tot(l, B, Dmin, Z):
 
-    spec = np.loadtxt(f"{RESULTS_DIR}/spec_{PARTICLES[iZ(Z)]}_EGMF_Dmin_{Dmin}Mpc.dat")
+    spec = np.loadtxt(f"{RESULTS_DIR}/spec_{PARTICLES[iZ(Z)]}_EGMF_{format_filename_value(B)}nG_Dmin_{Dmin}Mpc.dat")
     spec = spec[:len(ES)]
 
     Phi_l_tot = np.zeros_like(ES)
@@ -67,7 +66,7 @@ def Phi_l_tot(l, Dmin, Z):
         Phi_l = np.zeros_like(ES)
         
         for iE in range(len(ES)):
-            Phi_l_temp = compute_sph_harmonics_coeffs(l, iE, rs, Z)
+            Phi_l_temp = compute_sph_harmonics_coeffs(l, B, iE, rs, Z)
             if np.isfinite(Phi_l_temp):
                 Phi_l[iE] = Phi_l_temp
 
@@ -76,29 +75,43 @@ def Phi_l_tot(l, Dmin, Z):
     return spec[:,0], np.sqrt(Phi_l_tot)
 
 # ----------------------------------------------------------------------------------------------------
-def Phi_0_tot(Dmin, Z):
+def Phi_0_tot(B, Dmin, Z):
 
-    spec = np.loadtxt(f"{RESULTS_DIR}/spec_{PARTICLES[iZ(Z)]}_EGMF_Dmin_{Dmin}Mpc.dat")
+    spec = np.loadtxt(f"{RESULTS_DIR}/spec_{PARTICLES[iZ(Z)]}_EGMF_{format_filename_value(B)}nG_Dmin_{Dmin}Mpc.dat")
     spec = spec[:len(ES)]
     return spec[:,0], np.sum(spec[:, 1:], axis = 1)
 
 # ----------------------------------------------------------------------------------------------------
-def write_sph_coeffs_srcs(l, Dmin, Z):
+def format_filename_value(B):
+
+    B = float(B)
+
+    if B.is_integer():
+        return str(int(B))
+    else:
+        return str(B).replace('.', '_')
+    
+# ----------------------------------------------------------------------------------------------------
+def write_sph_coeffs_srcs(l, B, Dmin, Z):
 
     if l == 0:
-        E, coeffs = Phi_0_tot(Dmin, Z)
-        np.savetxt(f"{RESULTS_DIR}/Figures6to8/Phi_{l}_tot_{Dmin}Mpc_{PARTICLES[iZ(Z)]}_{B}nG.dat", np.column_stack((E, coeffs)), fmt = "%.15e")
+        E, coeffs = Phi_0_tot(B, Dmin, Z)
+        np.savetxt(f"{RESULTS_DIR}/Figures6to8/Phi_{l}_tot_{Dmin}Mpc_{PARTICLES[iZ(Z)]}_{format_filename_value(B)}nG.dat", np.column_stack((E, coeffs)), fmt = "%.15e")
 
     else:
-        E, coeffs = Phi_l_tot(l, Dmin, Z)
-        np.savetxt(f"{RESULTS_DIR}/Figures6to8/Phi_{l}_tot_{Dmin}Mpc_{PARTICLES[iZ(Z)]}_{B}nG.dat", np.column_stack((E, coeffs)), fmt = "%.15e")
+        E, coeffs = Phi_l_tot(l, B, Dmin, Z)
+        np.savetxt(f"{RESULTS_DIR}/Figures6to8/Phi_{l}_tot_{Dmin}Mpc_{PARTICLES[iZ(Z)]}_{format_filename_value(B)}nG.dat", np.column_stack((E, coeffs)), fmt = "%.15e")
 
 # ----------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
 
-    for Z in ZS:
-        for Dmin in [3, 9, 27, 81, 243]:
-            write_sph_coeffs_srcs(0, Dmin, Z)
-            write_sph_coeffs_srcs(1, Dmin, Z)
+    # for Z in ZS:
+    #     for Dmin in [3, 9, 27, 81, 243]:
+    #         write_sph_coeffs_srcs(0, 1, Dmin, Z)
+    #         write_sph_coeffs_srcs(1, 1, Dmin, Z)
+
+    for B in [0.1, 0.3, 3]:
+        write_sph_coeffs_srcs(0, B, 3, 7)
+        # write_sph_coeffs_srcs(1, B, 3, 7)
 
 # ----------------------------------------------------------------------------------------------------
