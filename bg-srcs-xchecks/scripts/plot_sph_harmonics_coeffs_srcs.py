e3b29bd0b4d95e8018c2c6681ddb461a8d50dd98
Luciana Andrade Dourado <luciana.dourado@gssi.it>
2025-08-14 12:22:40 +0200

    Add plot of dipole amplitude for different magnetic fields

--- bg-srcs-xchecks/scripts/plot_sph_harmonics_coeffs_srcs.py
+++ bg-srcs-xchecks/scripts/plot_sph_harmonics_coeffs_srcs.py
@@ -27,22 +27,32 @@ def iZ(Z):
         raise ValueError(f"Z ({Z}) not found in ZS.")
     
 # ----------------------------------------------------------------------------------------------------
-def get_color(iDmin, Z):
+def format_filename_value(B):
+
+    B = float(B)
+
+    if B.is_integer():
+        return str(int(B))
+    else:
+        return str(B).replace('.', '_')
+    
+# ----------------------------------------------------------------------------------------------------
+def get_color(i, Z):
 
     if Z == 1:
-        return cm.PuRd(np.linspace(0, 1, 10)[9 - iDmin])
+        return cm.PuRd(np.linspace(0, 1, 10)[9 - i])
     
     elif Z == 2:
-        return cm.BuGn(np.linspace(0, 1, 10)[9 - iDmin])
+        return cm.BuGn(np.linspace(0, 1, 10)[9 - i])
     
     elif Z == 7:
-        return cm.OrRd(np.linspace(0, 1, 10)[9 - iDmin])
+        return cm.OrRd(np.linspace(0, 1, 10)[9 - i])
     
     elif Z == 14:
-        return cm.GnBu(np.linspace(0, 1, 10)[9 - iDmin])
+        return cm.GnBu(np.linspace(0, 1, 10)[9 - i])
     
     elif Z == 26:
-        return cm.BuPu(np.linspace(0, 1, 10)[9 - iDmin])
+        return cm.BuPu(np.linspace(0, 1, 10)[9 - i])
 
 # ----------------------------------------------------------------------------------------------------
 def plot_sph_harmonics_coeffs_Fig7():
@@ -65,6 +75,29 @@ def plot_sph_harmonics_coeffs_Fig7():
     plt.legend(title = 'Nucleus')
     plt.savefig(f"{FIGURES_DIR}/sph_harmonics_coeffs_Fig7.pdf", bbox_inches = 'tight')
     plt.savefig(f"{FIGURES_DIR}/sph_harmonics_coeffs_Fig7.png", bbox_inches = 'tight', dpi = 300)
+    plt.show()
+
+# ----------------------------------------------------------------------------------------------------
+def plot_sph_harmonics_coeffs_Fig8():
+
+    for iB, B in enumerate([0.1, 0.3, 1, 3]):
+        Phi_0_tot = np.loadtxt(f"{RESULTS_DIR}/Figures6to8/Phi_0_tot_3Mpc_14N_{format_filename_value(B)}nG.dat")
+        Phi_1_tot = np.loadtxt(f"{RESULTS_DIR}/Figures6to8/Phi_1_tot_3Mpc_14N_{format_filename_value(B)}nG.dat")
+        plt.plot(np.log10(Phi_0_tot[:,0]), Phi_1_tot[:,1] / Phi_0_tot[:,1], c = get_color(2*iB, 7), label = f'{B}')
+
+        Lang2021_data = np.loadtxt(f"{REFERENCES_DIR}/Lang2021_dlt_log10E_3Mpc_14N_{format_filename_value(B)}nG.dat")
+        plt.plot(Lang2021_data[:,0], Lang2021_data[:,1], c = get_color(2*iB, 7), ls = '--', label  = '__nolegend__')
+
+    LAD_lgnd = lines.Line2D([], [], c = 'k', ls = '-', label = r'LAD')
+    Lang2021_lgnd = lines.Line2D([], [], c = 'k', ls = '--', label = r'Lang et al. (2021)')
+    plt.gca().add_artist(plt.legend(handles = [LAD_lgnd, Lang2021_lgnd], frameon = True, loc = 'upper left', bbox_to_anchor = (0.28, 1.)))
+
+    plt.yscale("log")
+    plt.xlabel(r"$\log_{10}{(\rm Energy / eV)}$")
+    plt.ylabel(r"Dipole amplitude")
+    plt.legend(title = 'Magnetic field')
+    plt.savefig(f"{FIGURES_DIR}/sph_harmonics_coeffs_Fig8.pdf", bbox_inches = 'tight')
+    plt.savefig(f"{FIGURES_DIR}/sph_harmonics_coeffs_Fig8.png", bbox_inches = 'tight', dpi = 300)
     plt.show()
 
 # ----------------------------------------------------------------------------------------------------
@@ -94,8 +127,10 @@ def plot_sph_harmonics_coeffs_Fig9(Z):
 if __name__ == '__main__':
 
     # plot_sph_harmonics_coeffs_Fig7()
+
+    plot_sph_harmonics_coeffs_Fig8()
     
-    for Z in ZS:
-        plot_sph_harmonics_coeffs_Fig9(Z)
+    # for Z in ZS:
+    #     plot_sph_harmonics_coeffs_Fig9(Z)
 
 # ----------------------------------------------------------------------------------------------------
