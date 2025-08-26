c17fa6a7b6c114677dbd7e900bc3ee698101c928
Luciana Andrade Dourado <luciana.dourado@gssi.it>
2025-08-26 17:30:58 +0200

    Add plot of dipole amplitude for background sources at several number densities

---
+++ background-sources/scripts/plot_sph_harmonics_coeffs.py
@@ -0,0 +1,75 @@
+from matplotlib import pyplot as plt
+from matplotlib.pylab import cm
+import numpy as np
+
+plt.rcParams.update({'legend.fontsize': 'large',
+'legend.title_fontsize': 'large',
+'axes.labelsize': 'x-large',
+'axes.titlesize': 'xx-large',
+'xtick.labelsize': 'x-large',
+'ytick.labelsize': 'x-large'})
+
+FIGURES_DIR = "../figures"
+REFERENCES_DIR = "../references"
+RESULTS_DIR = "../results"
+
+PARTICLES = ['1H', '4He', '14N', '28Si', '56Fe']
+PARTICLES_LEGEND = [r'$^1\mathrm{H}$', r'$^4\mathrm{He}$', r'$^{14}\mathrm{N}$', r'$^{28}\mathrm{Si}$', r'$^{56}\mathrm{Fe}$']
+ZS = [1, 2, 7, 14, 26]
+
+# ----------------------------------------------------------------------------------------------------
+def iZ(Z):
+
+    try:
+        return ZS.index(Z)
+    except ValueError:
+        raise ValueError(f"Z ({Z}) not found in ZS.")
+    
+# ----------------------------------------------------------------------------------------------------
+def get_color(i, Z):
+
+    if Z == 1:
+        return cm.PuRd(np.linspace(0, 1, 10)[9 - i])
+    
+    elif Z == 2:
+        return cm.BuGn(np.linspace(0, 1, 10)[9 - i])
+    
+    elif Z == 7:
+        return cm.OrRd(np.linspace(0, 1, 10)[9 - i])
+    
+    elif Z == 14:
+        return cm.GnBu(np.linspace(0, 1, 10)[9 - i])
+    
+    elif Z == 26:
+        return cm.BuPu(np.linspace(0, 1, 10)[9 - i])
+
+# ----------------------------------------------------------------------------------------------------
+def plot_sph_harmonics_coeffs(Dshell):
+
+    for Z in ZS:
+        
+        Phi_0_tot = np.loadtxt(f"{RESULTS_DIR}/{int(Dshell)}Mpc/Phi_0_tot_{PARTICLES[iZ(Z)]}.dat")
+        Phi_1_tot = np.loadtxt(f"{RESULTS_DIR}/{int(Dshell)}Mpc/Phi_1_tot_{PARTICLES[iZ(Z)]}.dat")
+
+        dlt = Phi_1_tot[:,1] / Phi_0_tot[:,1]
+        mask = (dlt > 0) & np.isfinite(dlt) 
+
+        plt.plot(np.log10(Phi_0_tot[:,0][mask]), dlt[mask], c = get_color(2, Z), label = PARTICLES_LEGEND[iZ(Z)])
+
+    plt.yscale("log")
+    plt.xlim([18, 20])
+    plt.ylim([1e-5, 3])
+    plt.xlabel(r"$\log_{10}{(\rm Energy / eV)}$")
+    plt.ylabel(r"Dipole amplitude")
+    plt.legend(title = 'Nucleus')
+    plt.savefig(f"{FIGURES_DIR}/sph_harmonics_coeffs_{int(Dshell)}Mpc.pdf", bbox_inches = 'tight')
+    plt.savefig(f"{FIGURES_DIR}/sph_harmonics_coeffs_{int(Dshell)}Mpc.png", bbox_inches = 'tight', dpi = 300)
+    plt.show()
+
+# ----------------------------------------------------------------------------------------------------
+if __name__ == '__main__':
+   
+   for Dshell in range(1, 10):
+        plot_sph_harmonics_coeffs(Dshell)
+
+# ----------------------------------------------------------------------------------------------------
