d7be8592aed63bc37beb78977be4f48dae72788f
Luciana Andrade Dourado <luciana.dourado@gssi.it>
2025-10-17 16:12:06 +0200

    Compute dipole amplitude for background sources summed over primaries

---
+++ background-sources/scripts/compute_sph_harmonics_all_Z.py
@@ -0,0 +1,102 @@
+from scipy.integrate import simps
+from scipy.special import legendre
+import numpy as np
+
+RESULTS_DIR = "../results"
+SIMULATIONS_DIR = "../../simulations/background-sources/results/angular-distr"
+
+ES = np.logspace(4. / (2. * 79), 4. - 4. / (2. * 79), num = 79)
+PARTICLES = ['1H', '4He', '14N', '28Si', '56Fe']
+ZSS = [1, 2, 7, 14, 26]
+
+B = 1 # nG
+lbd_coh = 1 # Mpc
+
+# ----------------------------------------------------------------------------------------------------
+def iZs(Zs):
+
+    try:
+        return ZSS.index(Zs)
+    except ValueError:
+        raise ValueError(f"Zs ({Zs}) not found in ZSS.")
+    
+# ----------------------------------------------------------------------------------------------------
+def scattering_length(E, Zs):
+
+    RL = 3.5 * 1.081 / Zs * E / B # Mpc; E in EeV and B in nG 
+
+    if RL < lbd_coh:
+        return (RL/lbd_coh)**(1/3) * lbd_coh # Mpc
+    
+    else:
+        return (RL/lbd_coh)**2 * lbd_coh # Mpc  
+
+# ----------------------------------------------------------------------------------------------------
+def d2N_dAdt_bal(E, rs, Zs): # Equation (5)
+
+    return np.exp(- rs / scattering_length(E, Zs)) / (4 * np.pi * rs**2)
+
+# ----------------------------------------------------------------------------------------------------
+def compute_sph_harmonics_coeffs(l, iE, rs, Zs):
+
+    data = np.loadtxt(f"{SIMULATIONS_DIR}/{PARTICLES[iZs(Zs)]}/{int(rs)}Mpc/angular_distr_{iE:02d}.dat")
+
+    N = (simps(data[:,1], data[:,0]) + d2N_dAdt_bal(ES[iE], rs, Zs)) / 2
+    x, integrand = data[:,0], legendre(l)(data[:,0]) * data[:,1]        
+
+    return (2*l + 1) / 2 / N * (simps(integrand, x) + d2N_dAdt_bal(ES[iE], rs, Zs) * legendre(l)(1))
+
+# ----------------------------------------------------------------------------------------------------
+def Phi_l_tot(l, Dshell):
+
+    Phi_l_tot = np.zeros_like(ES)
+    
+    for Zs in ZSS:
+        data = np.loadtxt(f"{RESULTS_DIR}/{int(Dshell)}Mpc/spec_{PARTICLES[iZs(Zs)]}_EGMF.dat")
+
+        for irs, rs in enumerate(np.arange(27, 860, Dshell)):
+            
+            if irs + 1 >= data.shape[1]: # To understand
+                break
+            
+            Phi_l = np.zeros_like(ES)
+            
+            for iE in range(len(ES)):
+                Phi_l_temp = compute_sph_harmonics_coeffs(l, iE, rs, Zs)
+                if np.isfinite(Phi_l_temp):
+                    Phi_l[iE] = Phi_l_temp
+
+            Phi_l_tot += (Phi_l * data[:,irs+1] / (irs + 1))**2
+
+    return data[:,0], np.sqrt(Phi_l_tot)
+
+# ----------------------------------------------------------------------------------------------------
+def Phi_0_tot(Dshell):
+
+    Phi_0_tot = np.zeros_like(ES)
+
+    for Zs in ZSS:
+        data = np.loadtxt(f"{RESULTS_DIR}/{int(Dshell)}Mpc/spec_{PARTICLES[iZs(Zs)]}_EGMF.dat")
+        Phi_0_tot += np.sum(data[:, 1:], axis = 1)
+
+    return data[:,0], Phi_0_tot
+    
+# ----------------------------------------------------------------------------------------------------
+def write_sph_coeffs_srcs(l, Dshell):
+
+    if l == 0:
+        E, coeffs = Phi_0_tot(Dshell)
+        np.savetxt(f"{RESULTS_DIR}/{int(Dshell)}Mpc/Phi_{l}_tot.dat", np.column_stack((E, coeffs)), fmt = "%.15e")
+
+    else:
+        E, coeffs = Phi_l_tot(l, Dshell)
+        np.savetxt(f"{RESULTS_DIR}/{int(Dshell)}Mpc/Phi_{l}_tot.dat", np.column_stack((E, coeffs)), fmt = "%.15e")
+
+# ----------------------------------------------------------------------------------------------------
+if __name__ == '__main__':
+
+    for Dshell in range(1, 10):
+        write_sph_coeffs_srcs(0, Dshell)
+        write_sph_coeffs_srcs(1, Dshell)
+
+# ----------------------------------------------------------------------------------------------------
--- background-sources/scripts/plot_sph_harmonics_coeffs.py
+++ background-sources/scripts/plot_sph_harmonics_coeffs.py
@@ -1,6 +1,7 @@
 from matplotlib import pyplot as plt
 from matplotlib.offsetbox import AnchoredText
 from matplotlib.pylab import cm
+from matplotlib.ticker import FuncFormatter
 import numpy as np
 
 plt.rcParams.update({'legend.fontsize': 'x-large',
@@ -17,6 +18,14 @@ RESULTS_DIR = "../results"
 PARTICLES = ['1H', '4He', '14N', '28Si', '56Fe']
 PARTICLES_LEGEND = [r'$^1\mathrm{H}$', r'$^4\mathrm{He}$', r'$^{14}\mathrm{N}$', r'$^{28}\mathrm{Si}$', r'$^{56}\mathrm{Fe}$']
 ZS = [1, 2, 7, 14, 26]
+
+# ----------------------------------------------------------------------------------------------------
+def format_ticks(x, pos):
+
+    if x == int(x):
+        return str(int(x))
+    else:
+        return str(x)
 
 # ----------------------------------------------------------------------------------------------------
 def iZ(Z):
@@ -50,7 +59,7 @@ def number_density(Dshell):
     return 1e4 / (4 * np.pi * Dshell**3) # 1e-4 Mpc^-3
 
 # ----------------------------------------------------------------------------------------------------
-def plot_sph_harmonics_coeffs(Dshell):
+def plot_sph_harmonics_per_Z(Dshell):
 
     for Z in ZS:
         
@@ -74,9 +83,34 @@ def plot_sph_harmonics_coeffs(Dshell):
     plt.show()
 
 # ----------------------------------------------------------------------------------------------------
+def plot_sph_harmonics_all_Z():
+    
+    for Dshell in range(1, 10):
+        Phi_0_tot = np.loadtxt(f"{RESULTS_DIR}/{Dshell}Mpc/Phi_0_tot.dat")
+        Phi_1_tot = np.loadtxt(f"{RESULTS_DIR}/{Dshell}Mpc/Phi_1_tot.dat")
+
+        dlt = Phi_1_tot[:,1] / Phi_0_tot[:,1]
+        mask = (dlt > 0) & np.isfinite(dlt)
+        
+        plt.plot(np.log10(Phi_0_tot[:,0][mask]), dlt[mask], c = plt.get_cmap('viridis')(np.linspace(0, 1, 10)[Dshell]), label = f'{number_density(Dshell):.2f}') 
+    
+    plt.gca().xaxis.set_major_formatter(FuncFormatter(format_ticks))
+    plt.yscale("log")
+    plt.xlim([18, 20])
+    plt.ylim([3e-5, 3])
+    plt.xlabel(r"$\log_{10}{(\rm Energy / eV)}$")
+    plt.ylabel(r"Dipole amplitude")
+    plt.legend(title = r'Number density$\: \rm [10^{-4} Mpc^{-3}]$', loc = 'lower center', ncol = 3)
+    plt.savefig(f"{FIGURES_DIR}/sph_harmonics_coeffs.pdf", bbox_inches = 'tight')
+    plt.savefig(f"{FIGURES_DIR}/sph_harmonics_coeffs.png", bbox_inches = 'tight', dpi = 300)
+    plt.show()
+    
+# ----------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
    
-   for Dshell in range(1, 10):
-        plot_sph_harmonics_coeffs(Dshell)
+#    for Dshell in range(1, 10):
+#         plot_sph_harmonics_per_Z(Dshell)
+
+    plot_sph_harmonics_all_Z()
 
 # ----------------------------------------------------------------------------------------------------
